e610329d8044fcea4bec96b2bfa0d4f5372ae82d
HamirDhanquer <hamirdhanquer@gmail.com>
2021-08-11 09:15:37 -0300

    Finalizando o projeto de API Restfull

--- 13-Flask_Restful/app/resources/contacts.py
+++ 13-Flask_Restful/app/resources/contacts.py
@@ -1,14 +1,53 @@
 from flask_restful import Resource, marshal
+from app.models import Contact
+from app import request, db
+from app.schemas import contact_field
 
 class Contacts(Resource):
+
     def get(self):
-        return {"message":"Helló Wõrld"}
+        contacts = Contact.query.all()
+        return marshal(contacts,contact_field,"contacts")
 
-    def post():
-        pass 
 
-    def put():
-        pass 
+    def post(self):
+        payload = request.only(["name","cellphone"]) 
+        name = payload["name"]
+        cellphone = payload["cellphone"]
 
-    def delete():
-        pass  
+        contacts = Contact(name,cellphone)
+        db.session.add(contacts)
+        db.session.commit()
+        return marshal(contacts,contact_field,"contacts")
+
+
+    def put(self):
+        payload = request.only(["id","name","cellphone"])
+        _id = payload["id"]
+        name=payload["name"]
+        cellphone = payload["cellphone"]
+
+        contact = Contact.query.get(_id)
+        
+        if not contact:
+            return {"message":"O Id requisitado não existe"}
+
+        contact.name = name
+        contact.phone = cellphone
+
+        db.session.add(contact)
+        db.session.commit()
+        return marshal(contact, contact_field, "contact")
+
+    def delete(self):
+        payload = request.only(["id"])
+        _id = payload["id"]
+
+        contact = Contact.query.get(_id)
+
+        if not contact:
+            return {"message":"O Id requisitado não existe"}
+
+        db.session.delete(contact)
+        db.session.commit()
+        return marshal(contact,contact_field,"contact")
